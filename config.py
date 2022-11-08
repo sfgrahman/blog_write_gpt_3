1c74caaffd86098b912b8c884b2625875e9943a5
Moududur Shamim <sfgrahman35@gmail.com>
2022-11-08 22:37:28 +0600

    AI blog writer with gpt-3

---
+++ config.py
@@ -0,0 +1,20 @@
+from decouple import config
+##OPEN API STUFF
+OPENAI_API_KEY = config('OPENAI_API_KEY')
+
+
+
+## FLASK STUFF
+class Config(object):
+    DEBUG = True
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    SECRET_KEY = "this-is-a-super-secret-key"
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': DevelopmentConfig,
+    'production': DevelopmentConfig
+}
